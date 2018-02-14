ac9390d38e52a4dcd3d23ee8d9b0b2207a11249c
Aadi1995 <aadityagupta12345@gmail.com>
2018-02-14 23:14:11 +0530

    Time Service

--- Buenos_time/server/service.js
+++ Buenos_time/server/service.js
@@ -4,22 +4,22 @@ service=express(),
 request=require('superagent'),
 moment=require('moment');
 
-//geocoding AIzaSyCF2T2LBwjAWoP-I1qs3xQt0BNBwIWjRQU
+//geocoding XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-//https://maps.googleapis.com/maps/api/geocode/json?address=London&key=AIzaSyCF2T2LBwjAWoP-I1qs3xQt0BNBwIWjRQU
+//https://maps.googleapis.com/maps/api/geocode/json?address=London&key=XXXXXXXXXXXXXXXXXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXX
 
-// time AIzaSyCWGKZorsZYaeP1BV7NvtN6TWuimvATzFk
-//https://maps.googleapis.com/maps/api/timezone/json?location=38.908133,-77.047119&timestamp=1458000000&key=AIzaSyCWGKZorsZYaeP1BV7NvtN6TWuimvATzFk
+// time XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//https://maps.googleapis.com/maps/api/timezone/json?location=38.908133,-77.047119&timestamp=1458000000&key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 service.get('/service/:location',(req,res,next)=>{
-    request.get('https://maps.googleapis.com/maps/api/geocode/json?address=' +  req.params.location +'&key=AIzaSyCF2T2LBwjAWoP-I1qs3xQt0BNBwIWjRQU',(err,response)=>{
+    request.get('https://maps.googleapis.com/maps/api/geocode/json?address=' +  req.params.location +'&key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',(err,response)=>{
         if(err){
             console.log(err)
             return res.sendStatus(500);
         }
         const location=response.body.results[0].geometry.location;
         const timestamp=+moment().format('X');
-        request.get('https://maps.googleapis.com/maps/api/timezone/json?location='+location.lat+','+location.lng+'&timestamp='+timestamp+'&key=AIzaSyCWGKZorsZYaeP1BV7NvtN6TWuimvATzFk',(err,response)=>{
+        request.get('https://maps.googleapis.com/maps/api/timezone/json?location='+location.lat+','+location.lng+'&timestamp='+timestamp+'&key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',(err,response)=>{
             if(err){
                 console.log(err);
                 res.sendStatus(500);
